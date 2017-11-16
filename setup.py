04e02f1d2138888e9f57eb0a859ff49176a58b0f
307977586@qq.com <307977586@qq.com>
2017-11-16 21:26:25 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
-  name = 'mypackage',
-  packages = ['mypackage'], # this must be the same as the name above
+  name = 'pyplotz',
+  packages = ['pyplotz'], # this must be the same as the name above
   version = '0.1',
-  description = 'A random test lib',
-  author = 'Peter Downs',
-  author_email = 'peterldowns@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  description = 'A light weight wrapper for matplotlib users with Chinese characters supported',
+  author = 'Ray Williams',
+  author_email = 'rock307977586@gmail.com',
+  url = 'https://github.com/201528015329004/pyplotz', # use the URL to the github repo
   download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
